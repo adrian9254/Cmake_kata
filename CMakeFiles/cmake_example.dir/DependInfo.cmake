7048caa10c69bedb18b0e3186aaa97a8fe0f8f8b
Adrian <adrian.podsiadlowski@gmail.com>
2020-06-07 14:06:52 +0200

    simple cmake usage

---
+++ CMakeFiles/cmake_example.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/ap/Notes/NotepadsGuides/Kata_calculator_cmake/main.cpp" "/home/ap/Notes/NotepadsGuides/Kata_calculator_cmake/CMakeFiles/cmake_example.dir/main.o"
+  "/home/ap/Notes/NotepadsGuides/Kata_calculator_cmake/modules/equations.cpp" "/home/ap/Notes/NotepadsGuides/Kata_calculator_cmake/CMakeFiles/cmake_example.dir/modules/equations.o"
+  "/home/ap/Notes/NotepadsGuides/Kata_calculator_cmake/modules/parser.cpp" "/home/ap/Notes/NotepadsGuides/Kata_calculator_cmake/CMakeFiles/cmake_example.dir/modules/parser.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CMakeFiles/cmake_example.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cmake_example.dir/main.o"
+  "CMakeFiles/cmake_example.dir/modules/equations.o"
+  "CMakeFiles/cmake_example.dir/modules/parser.o"
+  "cmake_example"
+  "cmake_example.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cmake_example.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
